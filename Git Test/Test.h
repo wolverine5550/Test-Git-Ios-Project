5b8d58ee4a690277d7a84bff53d74e92fe6e4ada
Adam Pascarella <apascar@gmail.com>
2015-06-29 22:56:03 -0400

    added a test class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Adam Pascarella on 6/29/15.
+//  Copyright (c) 2015 Adam Pascarella. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Adam Pascarella on 6/29/15.
+//  Copyright (c) 2015 Adam Pascarella. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
